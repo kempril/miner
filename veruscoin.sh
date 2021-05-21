52cd3bd814708d1213617994952d30372e4e88d1
kempril <84558961+kempril@users.noreply.github.com>
2021-05-21 13:13:07 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RW8KmsY5US8kf96s8DXxmNN2iRRxC6E2TU.worke2 -p x --cpu 4
